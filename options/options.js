d0776e41307cb4a5bea1b0f78f0b460d57600a90
Crowdbotics <team@crowdbotics.com>
2021-09-17 18:17:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "quiet_cell_30646Identifier",
+  url: "https://quiet_cell_30646Identifier.botics.co",
+  api: "https://quiet_cell_30646Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
